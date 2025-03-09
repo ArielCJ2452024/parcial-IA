f7c061893b5ebfc232a4c3f47b00d0052b79b30d
Ariel <arielcuebasajgamer@gmail.com>
2025-03-09 18:16:17 -0400

    arregle font incompatible con los ficheros

--- pantalla_inicio_final.py
+++ pantalla_inicio_final.py
@@ -3,7 +3,7 @@ import pygame
 def mostrar_pantalla_de_inicio(pantalla):
 
     pantalla.fill((0,0,0)) #fondo color negro
-    fuente = pygame.font.Font(None,50)
+    fuente = pygame.font.Font(None,36)
     texto=fuente.render("precione para iniciar",True, (255,255,255))
     rect_texto = texto.get_rect(center=(pantalla.get_width() // 2, pantalla.get_height() // 2))
 
